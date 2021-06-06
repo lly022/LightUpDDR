db8e1cc92fe1880fdb7afbd6572aaac37b08df50
lly022 <56405110+lly022@users.noreply.github.com>
2021-06-06 16:12:54 -0700

    Update lly022_lab11_part1.c

--- lly022_lab11_part1.c
+++ lly022_lab11_part1.c
@@ -14,6 +14,7 @@
 #include "simAVRHeader.h"
 #endif
 
+//timer variables and functions for atmega1284p microcontroller
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
@@ -69,6 +70,7 @@ typedef struct task{
 } task;
 
 //global variables
+unsigned char Mode = 0x00; //assign one for easy mode, two for hard mode
 unsigned char easyMode = 0x00;
 unsigned char hardMode = 0x00;
 unsigned char gameDone = 0x00;
@@ -79,7 +81,7 @@ unsigned char incScore = 0x00;
 unsigned char j = 0x00;
 unsigned char k = 0x00;
 
-//fourty elements
+//fourty elements for easy mode
 unsigned char arrEasyModeC[40] =
 {0xFF, 0xFF, 0xFF, 0x80, 0x01, 0x80, 0xFF, 0xFF, 0xFF, 0x80,
  0xFF, 0x80, 0x01, 0xFF, 0x01, 0xFF, 0x80, 0xFF, 0xFF, 0xFF,
@@ -92,7 +94,7 @@ unsigned char arrEasyModeD[40] =
  0xFE, 0xEF, 0xFE, 0xE0, 0xE0, 0xE0, 0xEF, 0xFE, 0xEF, 0xE0,
  0xFE, 0xE0, 0xE0, 0xEF, 0xE0, 0xFE, 0xE0, 0xEF, 0xFE, 0xEF};
 
-//one hundred elements 
+//one hundred elements for hard mode
 unsigned char arrHardModeC[100] =
 {0xFF, 0xFF, 0xFF, 0x80, 0x01, 0x80, 0xFF, 0xFF, 0xFF, 0x80,
  0xFF, 0x80, 0x01, 0xFF, 0x01, 0xFF, 0x80, 0xFF, 0xFF, 0xFF,
@@ -105,7 +107,7 @@ unsigned char arrHardModeC[100] =
  0xFF, 0xFF, 0xFF, 0x80, 0x01, 0x80, 0xFF, 0xFF, 0xFF, 0x80,
  0xFF, 0x80, 0x01, 0xFF, 0x01, 0xFF, 0x80, 0xFF, 0xFF, 0xFF};
 
-unsigned char arrHardModeD[] =
+unsigned char arrHardModeD[100] =
 {0xFE, 0xEF, 0xFE, 0xE0, 0xE0, 0xE0, 0xEF, 0xFE, 0xEF, 0xE0,
  0xFE, 0xE0, 0xE0, 0xEF, 0xE0, 0xFE, 0xE0, 0xEF, 0xFE, 0xEF,
  0xFE, 0xEF, 0xFE, 0xE0, 0xE0, 0xE0, 0xEF, 0xFE, 0xEF, 0xE0,
@@ -120,10 +122,10 @@ unsigned char arrHardModeD[] =
 //compute and return player's accuracy after game is done
 double ComputeAccuracy(unsigned char numericalScore){
         double accPercentage = 0.00;
-        if(easyMode && !hardMode){
+        if(Mode == 0x01){ //easy
                 accPercentage = numericalScore/40.00;
         }
-        else if(hardMode && !easyMode){
+        else if(Mode = 0x02){ //hard
                 accPercentage = numericalScore/100.00;
         }
         else{
@@ -132,6 +134,7 @@ double ComputeAccuracy(unsigned char numericalScore){
         return accPercentage;
 }
 
+//based on global variable accPercentage, value that goes into PORTC is determined, shows score to player
 unsigned char DetermineC(double accPercentage){
         unsigned char tempC = 0x00;
         if(0 <= accPercentage && accPercentage <= 0.25){
@@ -152,6 +155,7 @@ unsigned char DetermineC(double accPercentage){
          return tempC;
 }
 
+//based on global variable accPercentage, value that goes into PORTD is determined, shows score to player
 unsigned char DetermineD(double accPercentage){
         unsigned char tempD = 0x00;
         if(0 <= accPercentage && accPercentage <= 0.25){
@@ -172,7 +176,7 @@ unsigned char DetermineD(double accPercentage){
          return tempD;
 }
 
-//starter SM
+//starter SM - start menu, choose difficulty mode, flashing of score displayed, go back to start menu so player can play again
 enum SM1_STATES { SM1_SMStart, SM1_Init, SM1_StartMenu, SM1_SelectEasy, SM1_SelectHard, SM1_GameOver, SM1_CompAcc, SM1_Wait, SM1_WaitA0, SM1_WaitA1, SM1_On1, SM1_Off1, SM1_On2, SM1_Off2, SM1_On3, SM1_Off3};
 int StarterSMFct(int state) {
    unsigned char A0 = ~PINA & 0x01;
@@ -366,12 +370,17 @@ int StarterSMFct(int state) {
          PORTD = 0xF9;
          easyMode = 0x00;
          hardMode = 0x00;
+         Mode = 0x00;
+         gameDone = 0x00;
+         numericalScore = 0x00;
          break;
       case SM1_SelectEasy:
          easyMode = 0x01;
+         Mode = 0x01;
          break;
       case SM1_SelectHard:
          hardMode = 0x01;
+         Mode = 0x02;
          break;
       case SM1_GameOver:
          PORTC = 0xFF;
@@ -538,6 +547,7 @@ int EasyModeFct(int state) {
       case SM2_Done:
          if (1) {
             state = SM2_WaitEasy;
+            easyMode = 0x00;
          }
          break;
       default:
@@ -599,14 +609,13 @@ int EasyModeFct(int state) {
       case SM2_Done:
          gameDone = 0x01;
          keepScore = 0x00;
-         easyMode = 0x00;
          j = 0x00;
          break;
    }
    return state;
 }
 
-//function for hard mode
+//SM for hard mode
 enum SM4_STATES { SM4_SMStart, SM4_WaitHard, SM4_FiveCount, SM4_FourCount, SM4_ThreeCount, SM4_TwoCount, SM4_OneCount, SM4_KeepScore, SM4_game, SM4_Done};
 int HardModeFct(int state) {
    static unsigned char i = 0x00;
@@ -706,6 +715,7 @@ int HardModeFct(int state) {
       case SM4_Done:
          if (1) {
             state = SM4_WaitHard;
+            hardMode = 0x00;
          }
          break;
       default:
@@ -767,14 +777,13 @@ int HardModeFct(int state) {
       case SM4_Done:
          gameDone = 0x01;
          keepScore = 0x00;
-         hardMode = 0x00;
          j = 0x00;
          break;
    }
    return state;
 }
 
-//function keeps track of current score
+//SM keeps track of current score
 enum SM3_STATES { SM3_SMStart, SM3_WaitKeep, SM3_Check, SM3_WaitA0, SM3_WaitA1, SM3_WaitA2, SM3_WaitA3, SM3_RelA0, SM3_RelA1, SM3_RelA2, SM3_RelA3};
 int KeepScoreFct(int state) {
    unsigned char A0 = ~PINA & 0x01;
@@ -891,7 +900,6 @@ int KeepScoreFct(int state) {
 
          break;
       case SM3_WaitKeep:
-         numericalScore = 0x00;
          break;
       case SM3_Check:
          incScore = 0x00;
@@ -970,7 +978,6 @@ int KeepScoreFct(int state) {
 
 int main(void) {
     DDRA = 0x00; PORTA = 0xFF; //input
-    DDRB = 0xFF; PORTB = 0x00; //output for testing 
     DDRC = 0xFF; PORTC = 0x00; //output
     DDRD = 0xFF; PORTD = 0x00; //output
     static task task1;
@@ -986,7 +993,7 @@ int main(void) {
     task1.TickFct = &StarterSMFct;
 
     task2.state = SM2_SMStart;
-    task2.period = 500;
+    task2.period = 500; //easy mode period
     task2.elapsedTime = task2.period;
     task2.TickFct = &EasyModeFct;
 
@@ -996,7 +1003,7 @@ int main(void) {
     task3.TickFct = &KeepScoreFct;
 
     task4.state = SM4_SMStart;
-    task4.period = 250;
+    task4.period = 250; //hard mode period
     task4.elapsedTime = task4.period;
     task4.TickFct = &HardModeFct;
 
@@ -1005,7 +1012,7 @@ int main(void) {
 
     unsigned short i;
     while (1) {
-        for(i = 0; i < numTasks; i++){
+        for(i = 0; i < numTasks; i++){ //task scheduler
                 if(tasks[i]->elapsedTime == tasks[i]->period){
                         tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
                         tasks[i]->elapsedTime = 0;
